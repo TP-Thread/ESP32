e48509934f55cc442a62de14abc9642f5d206c38
A-rtos <A-rtos@outlook.com>
2026-04-26 23:27:12 +0800

    clean project

--- ESP32S3/build/esp-idf/mbedtls/mbedtls/library/cmake_install.cmake
+++ ESP32S3/build/esp-idf/mbedtls/mbedtls/library/cmake_install.cmake
@@ -1,8 +1,8 @@
-# Install script for directory: D:/EIDE/ESP/v5.5.1/esp-idf/components/mbedtls/mbedtls/library
+# Install script for directory: D:/EIDE/Espressif/frameworks/esp-idf-v5.5.4/components/mbedtls/mbedtls/library
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/ESP32S3")
+  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/esp32s3")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
@@ -34,18 +34,18 @@ endif()
 
 # Set path to fallback-tool for dependency-resolution.
 if(NOT DEFINED CMAKE_OBJDUMP)
-  set(CMAKE_OBJDUMP "D:/EIDE/ESP/Tools/tools/xtensa-esp-elf/esp-14.2.0_20241119/xtensa-esp-elf/bin/xtensa-esp32s3-elf-objdump.exe")
+  set(CMAKE_OBJDUMP "D:/EIDE/Espressif/tools/xtensa-esp-elf/esp-14.2.0_20260121/xtensa-esp-elf/bin/xtensa-esp32s3-elf-objdump.exe")
 endif()
 
 if(CMAKE_INSTALL_COMPONENT STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
-  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/lib" TYPE STATIC_LIBRARY FILES "E:/MCU/ESP32/ESP32S3/build/esp-idf/mbedtls/mbedtls/library/libmbedcrypto.a")
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/lib" TYPE STATIC_LIBRARY FILES "E:/RTOS/ESP32/esp32s3/build/esp-idf/mbedtls/mbedtls/library/libmbedcrypto.a")
 endif()
 
 if(CMAKE_INSTALL_COMPONENT STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
-  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/lib" TYPE STATIC_LIBRARY FILES "E:/MCU/ESP32/ESP32S3/build/esp-idf/mbedtls/mbedtls/library/libmbedx509.a")
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/lib" TYPE STATIC_LIBRARY FILES "E:/RTOS/ESP32/esp32s3/build/esp-idf/mbedtls/mbedtls/library/libmbedx509.a")
 endif()
 
 if(CMAKE_INSTALL_COMPONENT STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
-  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/lib" TYPE STATIC_LIBRARY FILES "E:/MCU/ESP32/ESP32S3/build/esp-idf/mbedtls/mbedtls/library/libmbedtls.a")
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/lib" TYPE STATIC_LIBRARY FILES "E:/RTOS/ESP32/esp32s3/build/esp-idf/mbedtls/mbedtls/library/libmbedtls.a")
 endif()
 
--- ESP32S3/main/main.c
+++ ESP32S3/main/main.c
@@ -1,55 +1,17 @@
 /**
- ******************************************************************************
- * @file     main.c
- * @author   TP-Thread
- * @date     2025-09-11
- ******************************************************************************
+ * @file main.c
+ * @author A-rtos (A-rtos@outlook.com)
+ * @brief ESP32 Sample Project
+ * @version 0.1
+ * @date 2026-04-26
+ * 
+ * @copyright Copyright (c) 2026 A-rtos
+ * 
  */
 
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
-#include "nvs_flash.h"
-#include "esp_flash.h"
-#include "led.h"
-#include "exti.h"
-#include "uart.h"
+#include <stdio.h>
 
 void app_main(void)
 {
-    uint32_t flash_size;
-    uint8_t len = 0;
-    uint16_t times = 0;
-    unsigned char data[RX_BUF_SIZE] = {0};
 
-    nvs_flash_init();
-    led_init();
-    exti_init();
-    uart_init(115200);
-
-    esp_flash_get_size(NULL, &flash_size);
-    printf("FLASH size:%ld MB flash\n", flash_size / (1024 * 1024)); /* 获取FLASH大小并显示 */
-
-    while (1)
-    {
-        uart_get_buffered_data_len(UART0, (size_t *)&len); /* 获取环形缓冲区数据长度 */
-
-        if (len > 0)
-        {
-            memset(data, 0, RX_BUF_SIZE); /* 对缓冲区清零 */
-            printf("您发送的消息为:\r\n");
-            uart_read_bytes(UART0, data, len, 100);                                  /* 读数据 */
-            uart_write_bytes(UART0, (const char *)data, strlen((const char *)data)); /* 写数据 */
-        }
-        else
-        {
-            times++;
-
-            if (times % 100 == 0)
-            {
-                led_toggle();
-            }
-
-            vTaskDelay(10);
-        }
-    }
 }
