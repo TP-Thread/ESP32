47274726761ae16635c537f443ed32d538ed18cb
A-rtos <A-rtos@outlook.com>
2026-04-26 23:33:41 +0800

    refactor architecture

---
+++ esp32s3/main/main.c
@@ -0,0 +1,17 @@
+/**
+ * @file main.c
+ * @author A-rtos (A-rtos@outlook.com)
+ * @brief ESP32 Sample Project
+ * @version 0.1
+ * @date 2026-04-26
+ * 
+ * @copyright Copyright (c) 2026 A-rtos
+ * 
+ */
+
+#include <stdio.h>
+
+void app_main(void)
+{
+
+}
